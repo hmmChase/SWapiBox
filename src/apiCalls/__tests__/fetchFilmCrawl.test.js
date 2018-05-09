4e28521fc8e132c5a57703514512ae6a95a4e0f9
hmmChase <hmmchase@gmail.com>
2018-05-08 18:49:18 -0600

    Add test

--- src/apiCalls/__tests__/fetchFilmCrawl.test.js
+++ src/apiCalls/__tests__/fetchFilmCrawl.test.js
@@ -2,19 +2,25 @@ import { fetchFilmCrawl } from '../fetchFilmCrawl';
 import { doFetch } from '../doFetch';
 jest.mock('../doFetch.js');
 
-const mockFetchedFilmData = {
-  opening_crawl: 'Film Crawl',
-  title: 'Movie Title',
-  release_date: '1982-09-18'
-};
-
-const mockCleanFilmData = {
-  crawl: mockFetchedFilmData.opening_crawl,
-  title: mockFetchedFilmData.title,
-  date: mockFetchedFilmData.release_date
-};
-
 describe('fetchFilmCrawl', () => {
+  let mockFetchedFilmData;
+  let mockCleanFilmData;
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    mockFetchedFilmData = {
+      opening_crawl: 'Film Crawl',
+      title: 'Movie Title',
+      release_date: '1982-09-18'
+    };
+
+    mockCleanFilmData = {
+      crawl: mockFetchedFilmData.opening_crawl,
+      title: mockFetchedFilmData.title,
+      date: mockFetchedFilmData.release_date
+    };
+  });
+
   it('matches snapshot', () => {
     expect(fetchFilmCrawl).toMatchSnapshot();
   });
@@ -26,9 +32,16 @@ describe('fetchFilmCrawl', () => {
     expect(doFetch).toHaveBeenCalledTimes(1);
   });
 
-  it('returns film crawl data ', async () => {
+  it('returns film crawl data if doFetch successful', async () => {
     doFetch.mockImplementation(() => mockFetchedFilmData);
 
     expect(await fetchFilmCrawl()).toEqual(mockCleanFilmData);
+  });
+
+  it('throws an error if doFetch fails', async () => {
+    const mockError = new Error('mock error');
+    doFetch.mockImplementation(() => Promise.reject(new Error('mock error')));
+
+    await expect(fetchFilmCrawl()).rejects.toEqual(mockError);
   });
 });
