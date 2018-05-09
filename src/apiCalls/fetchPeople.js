24c7f66bc04606732a8fa8f3e273f404eae401e0
Chase <hmmChase@users.noreply.github.com>
2018-05-08 21:59:37 -0600

    Add fetch people data functionality

--- src/apiCalls/fetchPeople.js
+++ src/apiCalls/fetchPeople.js
@@ -1,7 +1,45 @@
 import { doFetch } from './doFetch';
 
 export const fetchPeople = async () => {
-  const root = 'http://swapi.com/api';
-  const peopleData = await doFetch(`${root}/people`);
-  return peopleData;
+  try {
+    const root = 'https://swapi.co/api';
+    const peopleData = await doFetch(`${root}/people`);
+    const parsedPeopleData = peopleData.results.map(async person => {
+      console.log(person);
+
+      const homeWorldData = await fetchHomeWorld(person.homeworld);
+      const speciesData = await fetchSpecies(person.species);
+      return {
+        name: person.name,
+        ...homeWorldData,
+        ...speciesData
+      };
+    });
+    return await Promise.all(parsedPeopleData);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const fetchHomeWorld = async homeworldURL => {
+  try {
+    const homeWorldData = await doFetch(homeworldURL);
+    return {
+      homeWorld: homeWorldData.name,
+      population: homeWorldData.population
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const fetchSpecies = async speciesURL => {
+  try {
+    const speciesData = await doFetch(speciesURL);
+    return {
+      species: speciesData.name
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
