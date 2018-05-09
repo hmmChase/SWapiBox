99ce9023a689846f28efce1038dde57f68f47dee
hmmChase <hmmchase@gmail.com>
2018-05-09 17:53:20 -0600

    Move fetch call determination from App to CardContainer

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,22 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../Card/Card';
 import './CardContainer.css';
+import { fetchPeople } from '../../apiCalls/fetchPeople';
+import { fetchPlanets } from '../../apiCalls/fetchPlanets';
+import { fetchVehicles } from '../../apiCalls/fetchVehicles';
 
-const CardContainer = ({ categoryData }) => {
+class CardContainer extends Component {
+  constructor(props) {
+    super(props);
 
-  const dataCards = categoryData.map((dataObj, index) => {
-    return (
-      <Card
-        key={'card' + index}
-        dataObj={dataObj}
-      />
+    this.state = {
+      path: '',
+      people: [],
+      planets: [],
+      vehicles: [],
+      favorites: []
+    };
+  }
+  // console.log(match.path);
+
+  async componentDidMount() {
+    this.setState(
+      {
+        path: this.props.match.path
+      },
+      () => this.fetchCategory(this.state.path)
     );
-  })
+  }
 
+  // fetchCategory = async category => {
+  //   switch (category) {
+  //     case '/people':
+  //       if (this.state.people.length === 0) {
+  //         const categoryData = await fetchPeople(category);
+  //         await this.setState({
+  //           people: categoryData
+  //         });
+  //       }
+  //       break;
+  //     case '/planets':
+  //       if (this.state.planets.length === 0) {
+  //         const categoryData = await fetchPlanets(category);
+  //         this.setState({
+  //           planets: categoryData
+  //         });
+  //       }
+  //       break;
+  //     case '/vehicles':
+  //       if (this.state.vehicles.length === 0) {
+  //         const categoryData = await fetchVehicles(category);
+  //         this.setState({
+  //           vehicles: categoryData
+  //         });
+  //       }
+  //       break;
+  //     // case 'favorites':
+  //     //   return await fetchFavorites(category);
+  //     default:
+  //       break;
+  //   }
+  // };
 
+  dataCards = () => {
+    this.state['path'].map((dataObj, index) => {
+      return <Card key={'card' + index} dataObj={dataObj} />;
+    });
+  };
 
-
-  return <section className="Card-container">{dataCards}</section>;
-};
+  render() {
+    return <section className="Card-container" />;
+  }
+}
 
 export default CardContainer;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,10 +5,9 @@ import Header from '../../components/Header/Header';
 import CardContainer from '../../components/CardContainer/CardContainer';
 import FilmCrawl from '../FilmCrawl/FilmCrawl';
 
-import { fetchPeople } from '../../apiCalls/fetchPeople';
-import { fetchPlanets } from '../../apiCalls/fetchPlanets';
-import { fetchVehicles } from '../../apiCalls/fetchVehicles';
-
+// import { fetchPeople } from '../../apiCalls/fetchPeople';
+// import { fetchPlanets } from '../../apiCalls/fetchPlanets';
+// import { fetchVehicles } from '../../apiCalls/fetchVehicles';
 
 class App extends Component {
   constructor() {
@@ -19,70 +18,85 @@ class App extends Component {
       people: [],
       planets: [],
       vehicles: [],
-      favorites: [],
+      favorites: []
     };
   }
 
+  componentDidMount = () => {};
 
-  componentDidMount = () => { };
+  // setCategory = async category => {
+  //   // this.setState({
+  //   //   category,
+  //   //   [category]: await this.fetchCategory(category)
+  //   // });
+  //   this.fetchCategory(category)
+  // };
 
-
-  setCategory = async category => {
-    // this.setState({
-    //   category,
-    //   [category]: await this.fetchCategory(category)
-    // });
-    this.fetchCategory(category)
-  };
-
-
-  fetchCategory = async category => {
-    switch (category) {
-      case 'people':
-        if (this.state.people.length === 0) {
-          console.log('fetching')
-          const data = await fetchPeople(category);
-          this.setState({
-            [category]: data
-          });
-        }
-        break;
-      case 'planets':
-        if (this.state.planets.length === 0) {
-          console.log('fetching')
-          const data = await fetchPlanets(category);
-          this.setState({
-            [category]: data
-          });
-        }
-        break;
-      case 'vehicles':
-        if (this.state.vehicles.length === 0) {
-          console.log('fetching')
-          const data = await fetchVehicles(category);
-          this.setState({
-            [category]: data
-          });
-        }
-        break;
-      // case 'favorites':
-      //   return await fetchFavorites(category);
-      default:
-        break;
-    }
-  }
-
-
+  // fetchCategory = async category => {
+  //   switch (category) {
+  //     case 'people':
+  //       if (this.state.people.length === 0) {
+  //         console.log('fetching');
+  //         const data = await fetchPeople(category);
+  //         this.setState({
+  //           [category]: data
+  //         });
+  //       }
+  //       break;
+  //     case 'planets':
+  //       if (this.state.planets.length === 0) {
+  //         console.log('fetching');
+  //         const data = await fetchPlanets(category);
+  //         this.setState({
+  //           [category]: data
+  //         });
+  //       }
+  //       break;
+  //     case 'vehicles':
+  //       if (this.state.vehicles.length === 0) {
+  //         console.log('fetching');
+  //         const data = await fetchVehicles(category);
+  //         this.setState({
+  //           [category]: data
+  //         });
+  //       }
+  //       break;
+  //     // case 'favorites':
+  //     //   return await fetchFavorites(category);
+  //     default:
+  //       break;
+  //   }
+  // };
 
   render() {
     return (
       <main className="App">
         <Header setCategory={this.setCategory} />
         <Route exact path="/" component={FilmCrawl} />
-        <Route path="/people" render={() => <CardContainer categoryData={this.state.people} />} />
-        <Route path="/planets" render={() => <CardContainer categoryData={this.state.planets} />} />
-        <Route path="/vehicles" render={() => <CardContainer categoryData={this.state.vehicles} />} />
-        <Route path="/favorites" render={() => <CardContainer categoryData={this.state.favorites} />} />
+        <Route
+          path="/people"
+          render={({ match }) => (
+            <CardContainer match={match} categoryData={this.state.people} />
+          )}
+        />
+        <Route
+          path="/planets"
+          render={({ match }) => (
+            <CardContainer match={match} categoryData={this.state.planets} />
+          )}
+        />
+        <Route
+          path="/vehicles"
+          render={({ match }) => (
+            <CardContainer match={match} categoryData={this.state.vehicles} />
+          )}
+        />
+        <Route
+          path="/favorites"
+          render={({ match }) => (
+            <CardContainer match={match} categoryData={this.state.favorites} />
+          )}
+        />
       </main>
     );
   }
