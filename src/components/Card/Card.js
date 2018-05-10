90d63f18eb73da982cac00e53716fa356d7cd866
hmmChase <hmmchase@gmail.com>
2018-05-10 17:44:56 -0600

    Refactor h2 key

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './Card.css';
 
 const Card = ({ dataObj }) => {
-  const dataObjKeys = Object.keys(dataObj)
+  const dataObjKeys = Object.keys(dataObj);
 
   const dataElements = dataObjKeys.map((key, index) => {
     return key === 'name'
-      ? <h2 key={'name' + index}>{dataObj[key]}</h2>
+      ? <h2 key={key + index}>{dataObj[key]}</h2>
       : <p key={key + index}>{key}: <span>{dataObj[key]}</span></p>;
-  })
-
-
+  });
 
   return (
     <article>
