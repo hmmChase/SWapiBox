acd8a3c7a3028b9b97229af68507ccd42314deaa
Chase <hmmChase@users.noreply.github.com>
2018-05-11 08:29:54 -0600

    Add mock data

--- src/__mocks__/mockData.js
+++ src/__mocks__/mockData.js
@@ -191,3 +191,99 @@ export const fetchedSpeciesData = {
   skin_colors: 'caucasian, black, asian, hispanic',
   url: 'https://swapi.co/api/species/1/'
 };
+
+export const fetchedResidentsData = {
+  name: 'Leia Organa',
+  height: '150',
+  mass: '49',
+  hair_color: 'brown',
+  skin_color: 'light',
+  eye_color: 'brown',
+  birth_year: '19BBY',
+  gender: 'female',
+  homeworld: 'https://swapi.co/api/planets/2/',
+  films: [
+    'https://swapi.co/api/films/2/',
+    'https://swapi.co/api/films/6/',
+    'https://swapi.co/api/films/3/',
+    'https://swapi.co/api/films/1/',
+    'https://swapi.co/api/films/7/'
+  ],
+  species: ['https://swapi.co/api/species/1/'],
+  vehicles: ['https://swapi.co/api/vehicles/30/'],
+  starships: [],
+  created: '2014-12-10T15:20:09.791000Z',
+  edited: '2014-12-20T21:17:50.315000Z',
+  url: 'https://swapi.co/api/people/5/'
+};
+
+export const fetchedPlanetsData = {
+  count: 61,
+  next: 'https://swapi.co/api/planets/?page=2',
+  previous: null,
+  results: [
+    {
+      name: 'Alderaan',
+      rotation_period: '24',
+      orbital_period: '364',
+      diameter: '12500',
+      climate: 'temperate',
+      gravity: '1 standard',
+      terrain: 'grasslands, mountains',
+      surface_water: '40',
+      population: '2000000000',
+      residents: ['https://swapi.co/api/people/5/'],
+      films: ['https://swapi.co/api/films/6/', 'https://swapi.co/api/films/1/'],
+      created: '2014-12-10T11:35:48.479000Z',
+      edited: '2014-12-20T20:58:18.420000Z',
+      url: 'https://swapi.co/api/planets/2/'
+    }
+  ]
+};
+
+export const cleanResidentsData = 'Leia Organa';
+
+export const cleanPlanetsData = [
+  {
+    name: 'Alderaan',
+    terrain: 'grasslands, mountains',
+    population: '2000000000',
+    climate: 'temperate',
+    residents: 'Leia Organa'
+  }
+];
+
+export const fetchedVehicleData = {
+  count: 39,
+  next: 'https://swapi.co/api/vehicles/?page=2',
+  previous: null,
+  results: [
+    {
+      name: 'Sand Crawler',
+      model: 'Digger Crawler',
+      manufacturer: 'Corellia Mining Corporation',
+      cost_in_credits: '150000',
+      length: '36.8',
+      max_atmosphering_speed: '30',
+      crew: '46',
+      passengers: '30',
+      cargo_capacity: '50000',
+      consumables: '2 months',
+      vehicle_class: 'wheeled',
+      pilots: [],
+      films: ['https://swapi.co/api/films/5/', 'https://swapi.co/api/films/1/'],
+      created: '2014-12-10T15:36:25.724000Z',
+      edited: '2014-12-22T18:21:15.523587Z',
+      url: 'https://swapi.co/api/vehicles/4/'
+    }
+  ]
+};
+
+export const cleanVehicleData = [
+  {
+    name: 'Sand Crawler',
+    model: 'Digger Crawler',
+    class: 'wheeled',
+    passengers: '30'
+  }
+];
