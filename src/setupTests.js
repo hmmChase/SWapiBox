89ada439712df4006a1ce2b26e011a55c1fedd57
Chase <hmmChase@users.noreply.github.com>
2018-05-12 20:50:44 -0600

    Add mock localStorage

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,18 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+global.localStorage = {
+  setItem(key, string) {
+    global.localStorage[key] = string;
+  },
+  getItem(key) {
+    return global.localStorage[key] || null;
+  },
+  removeItem(key) {
+    delete global.localStorage[key];
+  },
+  clear() {
+    global.localStorage = {};
+  }
+};
